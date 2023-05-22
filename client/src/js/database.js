45f876e14c428b0ae60b8354f40fff8844e0b957
AleksandrBausher <bausheraleksandr@gmail.com>
2023-05-22 11:42:24 -0400

    configured indexdb database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const database = await openDB("jate", 1);
+  const transaction = database.transaction("jate", "readwrite");
+  const store = transaction.objectStore("jate");
+  const request = store.put({id:1, content: content});
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const database = await openDB("jate", 1);
+  const transaction = database.transaction("jate", "readonly");
+  const store = transaction.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+
+  if (result){
+    return request ['content']
+  }
+
+  return '';
+}
+
 
 initdb();
